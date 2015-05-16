0324aff03b5dc484f648693dace13c13a7e834c7
Iain Adams <iwarapter@hotmail.com>
2015-05-16 21:13:21 +0100

    Added in PITest

--- sonar-puppet-plugin/src/main/java/com/iadams/sonarqube/puppet/PuppetPlugin.java
+++ sonar-puppet-plugin/src/main/java/com/iadams/sonarqube/puppet/PuppetPlugin.java
@@ -65,7 +65,6 @@ public class PuppetPlugin extends SonarPlugin {
 
                 PuppetCommonRulesEngine.class,
 
-                //PuppetDefaultProfile.class,
                 PplintConfiguration.class,
                 PplintSensor.class,
                 PplintRuleRepository.class);
