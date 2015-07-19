5eeff86b0ae2a455500cffe9714011dc958935bd
David RACODON <david.racodon@gmail.com>
2015-07-19 10:59:45 +0200

    Issue 20 - Improve tests to check // comments

--- puppet-checks/src/test/resources/checks/TodoTagPresence.pp
+++ puppet-checks/src/test/resources/checks/TodoTagPresence.pp
@@ -18,4 +18,15 @@ notice('hello') /* TODO: blabla */
 # TODO:
 #   todo
 notice('hello') # TODO: blabla
-# todoblabla
+#
+
+//TODO
+// TODO
+// TODO:
+// comments
+// [TODO] blabla
+// TODO: blabla
+// TODO:
+//   todo
+notice('hello') // TODO: blabla
+// todoblabla
