775f0862e4587e488ca2a39c242b4722572698b4
Iain Adams <iwarapter@hotmail.com>
2015-08-04 22:59:47 +0100

    Added check for case statements without a default.

--- puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/CaseWithoutDefaultCheck.java
+++ puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/CaseWithoutDefaultCheck.java
@@ -25,10 +25,8 @@
 package com.iadams.sonarqube.puppet.checks;
 
 import com.iadams.sonarqube.puppet.api.PuppetGrammar;
-import com.iadams.sonarqube.puppet.lexer.PuppetLexer;
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.Grammar;
-import java.util.regex.Pattern;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
@@ -37,45 +35,32 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 import org.sonar.squidbridge.checks.SquidCheck;
 
-import static com.iadams.sonarqube.puppet.api.PuppetTokenType.DOUBLE_QUOTED_STRING_LITERAL;
-import static com.iadams.sonarqube.puppet.api.PuppetTokenType.SINGLE_QUOTED_STRING_LITERAL;
-
 @Rule(
-  key = "FileModes",
-  name = "File modes should be represented as 4 digits rather than 3 or symbolically.",
-  priority = Priority.MINOR,
-  tags = {Tags.CONVENTION})
-@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.FAULT_TOLERANCE)
-@SqaleConstantRemediation("10min")
+        key = "CaseWithoutDefault",
+        priority = Priority.INFO,
+        name = "Case statements should have default cases.",
+        tags = Tags.PITFALL
+)
 @ActivatedByDefault
-public class FileModeCheck extends SquidCheck<Grammar> {
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.FAULT_TOLERANCE)
+@SqaleConstantRemediation("1h")
+public class CaseWithoutDefaultCheck extends SquidCheck<Grammar> {
 
-  private static final String REGEX = "['|\"]?([0-7]{4}|([ugoa]*[-=+][-=+rstwxXugo]*)(,[ugoa]*[-=+][-=+rstwxXugo]*)*)['|\"]?";
-  private Pattern pattern = Pattern.compile(REGEX);
-
-  @Override
-  public void init() {
-    subscribeTo(PuppetGrammar.RESOURCE);
-  }
-
-  @Override
-  public void visitNode(AstNode node) {
-    if(node.getTokenValue().equals("file")) {
-      for (AstNode body : node.getDescendants(PuppetGrammar.RESOURCE_INST)) {
-        for (AstNode name : body.getDescendants(PuppetGrammar.PARAM)) {
-          if(name.getTokenValue().equals("mode")){
-            checkMode(name.getFirstChild(PuppetGrammar.EXPRESSION));
-          }
-        }
-      }
+    @Override
+    public void init() {
+        subscribeTo(PuppetGrammar.CASE_STMT);
     }
-  }
 
-  private void checkMode(AstNode node){
-	if(node.getToken().getType().equals(SINGLE_QUOTED_STRING_LITERAL) || node.getToken().getType().equals(DOUBLE_QUOTED_STRING_LITERAL)) {
-      if (!pattern.matcher(node.getTokenValue()).matches()) {
-        getContext().createLineViolation(this, "File modes should be represented as 4 digits rather than 3, to explicitly show that they are octal values.", node.getTokenLine());
-      }
-	}
-  }
+    @Override
+    public void visitNode(AstNode node) {
+        boolean hasDefault = false;
+		for(AstNode cases : node.getDescendants(PuppetGrammar.CASE_MATCHER)){
+			if(cases.getTokenValue().equals("default")){
+                hasDefault = true;
+            }
+		}
+        if(!hasDefault){
+            getContext().createLineViolation(this, "Case statements should have default cases.", node);
+        }
+    }
 }
--- puppet-checks/src/test/groovy/com/iadams/sonarqube/puppet/checks/CaseWithoutDefaultCheckSpec.groovy
+++ puppet-checks/src/test/groovy/com/iadams/sonarqube/puppet/checks/CaseWithoutDefaultCheckSpec.groovy
@@ -22,42 +22,25 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.iadams.sonarqube.puppet.checks;
+package com.iadams.sonarqube.puppet.checks
 
-import com.google.common.collect.ImmutableList;
+import com.iadams.sonarqube.puppet.PuppetAstScanner
+import org.sonar.squidbridge.api.SourceFile
+import org.sonar.squidbridge.checks.CheckMessagesVerifier
+import spock.lang.Specification
 
-import java.util.List;
+class CaseWithoutDefaultCheckSpec extends Specification {
 
-public final class CheckList {
+    private static final String MESSAGE = "Case statements should have default cases.";
 
-	public static final String REPOSITORY_KEY = "puppet";
+    def "validate check"() {
+        given:
+        final CaseWithoutDefaultCheck check = new CaseWithoutDefaultCheck();
+        SourceFile file = PuppetAstScanner.scanSingleFile(new File("src/test/resources/checks/case_without_default.pp"), check);
 
-	public static final String SONARQUBE_WAY_PROFILE = "SonarQube Way";
-
-	public static List<Class> getChecks() {
-		return ImmutableList.<Class>of(
-				CaseWithoutDefaultCheck.class,
-				CommentConventionCheck.class,
-				CommentRegularExpressionCheck.class,
-				DuplicatedParametersCheck.class,
-				EnsureOrderingCheck.class,
-				FileModeCheck.class,
-				FileNameCheck.class,
-				FixmeTagPresenceCheck.class,
-				ImportStatementUsedCheck.class,
-				LineLengthCheck.class,
-				MissingNewLineAtEndOfFileCheck.class,
-				NosonarTagPresenceCheck.class,
-				ParsingErrorCheck.class,
-				QuotedBooleanCheck.class,
-				ResourceDefaultUsedCheck.class,
-				TabCharacterCheck.class,
-				TodoTagPresenceCheck.class,
-				TrailingWhitespaceCheck.class,
-				UserResourceLiteralNameCheck.class,
-				UserResourcePasswordNotSetCheck.class,
-				VariableNamingConventionCheck.class,
-				XPathCheck.class
-		);
-	}
+        expect:
+        CheckMessagesVerifier.verify(file.getCheckMessages())
+                .next().atLine(13).withMessage(MESSAGE)
+                .noMore();
+    }
 }
