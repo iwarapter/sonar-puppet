457ada69fad3b8c3adc7b897fa2d1ca3fe3fe802
David RACODON <david.racodon@gmail.com>
2015-08-05 15:02:28 +0200

    Tag FilesModes as BUG

--- puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/FileModeCheck.java
+++ puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/FileModeCheck.java
@@ -44,7 +44,7 @@ import static com.iadams.sonarqube.puppet.api.PuppetTokenType.*;
   key = "FileModes",
   name = "File mode should be represented by a valid 4-digit octal value (rather than 3) or symbolically",
   priority = Priority.MINOR,
-  tags = {Tags.CONVENTION})
+  tags = {Tags.BUG})
 @SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.FAULT_TOLERANCE)
 @SqaleConstantRemediation("10min")
 @ActivatedByDefault
