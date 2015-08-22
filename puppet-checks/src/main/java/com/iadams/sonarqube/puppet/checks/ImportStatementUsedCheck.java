bfc1415b4a2c8af74b8e7b528c398e2bdc6164a5
David RACODON <david.racodon@gmail.com>
2015-08-22 15:42:56 +0200

    Add future-parser tag to import and node inheritance rules

--- puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/ImportStatementUsedCheck.java
+++ puppet-checks/src/main/java/com/iadams/sonarqube/puppet/checks/ImportStatementUsedCheck.java
@@ -38,7 +38,7 @@ import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
   key = "ImportStatementUsed",
   priority = Priority.CRITICAL,
   name = "Deprecated \"import\" statement should not be used",
-  tags = {Tags.OBSOLETE})
+  tags = {Tags.OBSOLETE, Tags.FUTURE_PARSER})
 @ActivatedByDefault
 @SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.LANGUAGE_RELATED_PORTABILITY)
 @SqaleConstantRemediation("30min")
